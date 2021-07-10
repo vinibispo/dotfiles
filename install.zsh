e51afc7097562c5a40617c878d5b6aa205fdc8aa
vinibispo <vini.bispo015@gmail.com>
2021-07-10 16:07:09 -0300

    removing log on install.zsh

--- install.zsh
+++ install.zsh
@@ -2,10 +2,8 @@
 DOTFILES=~/dotfiles
 pushd $DOTFILES
 STOW_FOLDERS="$(echo */)"
-echo $STOW_FOLDERS
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
-    "Installing $folder"
     stow -D $folder
     stow $folder
 done
