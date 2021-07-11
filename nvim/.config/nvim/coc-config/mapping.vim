89bf6d3776a7222bebda31dfb109b6e0cc33c261
vinibispo <vini.bispo015@gmail.com>
2021-07-10 22:12:07 -0300

    rewrite vimscript for lua language

--- nvim/.config/nvim/coc-config/mapping.vim
+++
@@ -1,5 +0,0 @@
-" Add `:Fold` command to fold current buffer.
-command! -nargs=? Fold :call     CocAction('fold', <f-args>)
-command! -nargs=0 Prettier :CocCommand prettier.formatFile
-vmap <leader>f  <Plug>(coc-format-selected)
-nmap <leader>f  <Plug>(coc-format-selected)
---
+++ nvim/.config/nvim/lua/modules/formatter.lua
@@ -0,0 +1,58 @@
+-- formatter modules
+local function prettier()
+  return {
+    exe = "prettier",
+    args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+    stdin = true,
+  }
+end
+
+require("formatter").setup({
+  logging = false,
+  filetype = {
+    lua = {
+      function()
+        return {
+          exe = "lua-format",
+          args = {"-c " .. vim.fn.expand("~/.config/nvim/lua/.lua-format")},
+          stdin = true,
+        }
+      end,
+    },
+    javascript = {prettier},
+    javascriptreact = {prettier},
+    markdown = {prettier},
+    typescript = {prettier},
+    typescriptreact = {prettier},
+    json = {
+      function()
+        return {
+          exe = "prettier",
+          args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), "--parser", "json"},
+          stdin = true,
+        }
+      end,
+    },
+    ruby = {
+      function()
+        return {
+          exe = "rubocop",
+          stdin = true,
+          args = {
+            "--stdin",
+            vim.api.nvim_buf_get_name(0),
+            "--stderr",
+            "--auto-correct-all",
+          },
+        }
+      end,
+    },
+  },
+})
+-- adding format on save autocmd
+vim.api.nvim_exec([[
+    augroup FormatAu
+        autocmd!
+        autocmd BufWritePost *.lua,*.json,*.js,*.jsx,*.ts,*.tsx,*.md FormatWrite
+    augroup END
+]], true)
