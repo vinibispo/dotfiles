6ddb09a377a94f638716aad258c3fa3806e1f9ad
vinibispo <vini.bispo015@gmail.com>
2021-09-18 19:06:04 -0300

    fix: fix neogit conflict and add more completions to cmp

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -20,7 +20,6 @@ neogit.setup {
     status = {
       -- Adds a mapping with "B" as key that does the "BranchPopup" command
       ["B"] = "BranchPopup",
-      -- Removes the default mapping of "s"
     },
   },
 }
