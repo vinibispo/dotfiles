071c78298dc901ef9bc8286e5b60f6499c8cf9c5
vinibispo <vini.bispo015@gmail.com>
2021-09-20 11:10:37 -0300

    feat: improving code on symlinks

--- symlinks.rb
+++ symlinks.rb
@@ -2,32 +2,35 @@
 
 directories = Dir
               .glob('*')
-              .map { |f| File.expand_path(f) }
-              .reject { |f| File.file?(f) }
+              .map(&File.method(:expand_path))
+              .reject(&File.method(:file?))
 
-directories.each do |dir|
-  puts dir
+directories.each do |directory_in_dotfiles|
+  puts directory_in_dotfiles
   location_of_folder =
-    if File.exist?("#{dir}/.location")
-      File.read("#{dir}/.location").gsub("\n", '')
+    if File.exist?("#{directory_in_dotfiles}/.location")
+      File.read("#{directory_in_dotfiles}/.location").gsub("\n", '')
     else
       '~'
     end
-  folder = File.expand_path(location_of_folder)
-  if File.exist?(folder)
-    if File.symlink?(folder)
-      File.unlink(folder)
-      File.symlink(dir, folder)
-    else
-      children_dir = Dir.children(dir)
-      children_dir.each do |file|
-        complete_path = "#{folder}/#{file}"
-        file_expanded = File.expand_path("#{dir}/#{file}")
-        File.unlink(complete_path) if File.symlink?(complete_path)
-        File.symlink(file_expanded, complete_path)
-      end
-    end
+
+  symlinked_folder = File.expand_path(location_of_folder)
+
+  unless File.exist?(symlinked_folder)
+    File.symlink(directory_in_dotfiles, symlinked_folder)
+    next
+  end
+
+  if File.symlink?(symlinked_folder)
+    File.unlink(symlinked_folder)
+    File.symlink(directory_in_dotfiles, symlinked_folder)
   else
-    File.symlink(dir, folder)
+    children_dir = Dir.children(directory_in_dotfiles)
+    children_dir.each do |file|
+      complete_path = "#{symlinked_folder}/#{file}"
+      file_expanded = File.expand_path("#{directory_in_dotfiles}/#{file}")
+      File.unlink(complete_path) if File.symlink?(complete_path)
+      File.symlink(file_expanded, complete_path)
+    end
   end
 end
