9fe71a3b1825532ec9076a28cf27ed1a092508d5
vinibispo <vini.bispo015@gmail.com>
2021-09-29 06:08:04 -0300

    feat: add zsh_stats

--- zsh/zsh_plugins/common_aliases.zsh
+++ zsh/zsh_plugins/common_aliases.zsh
@@ -13,7 +13,11 @@ alias ls='logo-ls'
 function mkcd takedir() {
 mkdir -p $@ && cd ${@:$#}
 }
-
+function zsh_stats() {
+  fc -l 1 \
+    | awk '{ CMD[$2]++; count++; } END { for (a in CMD) print CMD[a] " " CMD[a]*100/count "% " a }' \
+    | grep -v "./" | sort -nr | head -20 | column -c3 -s " " -t | nl
+}
 function takeurl() {
   local data thedir
   data="$(mktemp)"
