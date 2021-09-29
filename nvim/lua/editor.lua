59424c8ce59874e7d7d1bfb296504296268e88c9
vinibispo <vini.bispo015@gmail.com>
2021-09-29 06:15:39 -0300

    feat: remove old nvim-tree config and add telescope shortcut

--- nvim/lua/editor.lua
+++ nvim/lua/editor.lua
@@ -5,26 +5,6 @@ local cmd = vim.cmd
 
 local function set_globals()
   g.mapleader = ','
-  g.nvim_tree_width = 40
-  g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
-  g.nvim_tree_gitignore = 1
-  g.nvim_tree_auto_open = 1
-  g.nvim_tree_auto_close = 1
-  g.nvim_tree_quit_on_open = 1
-  g.nvim_tree_follow = 1
-  g.nvim_tree_indent_markers = 1
-  g.nvim_tree_hide_dotfiles = 1
-  g.nvim_tree_git_hl = 1
-  g.nvim_tree_highlight_opened_files = 1
-  g.nvim_tree_root_folder_modifier = ':~'
-  g.nvim_tree_tab_open = 1
-  g.nvim_tree_auto_resize = 0
-  g.nvim_tree_add_trailing = 1
-  g.nvim_tree_group_empty = 1
-  g.nvim_tree_lsp_diagnostics = 1
-  g.nvim_tree_disable_window_picker = 1
-  g.nvim_tree_icon_padding = ' '
-  g.nvim_tree_update_cwd = 1
 end
 local function set_options()
   local undo_dir = os.getenv("HOME") .. '/.local/share/nvim/undo'
@@ -130,6 +110,7 @@ local function set_mapping()
     -- {"i", "<left>", "<nop>", opts},
     -- {"i", "<right>", "<nop>", opts},
     -- stop c, s and d from yanking
+    {"n", ";", [[<Cmd>lua require 'plugins.telescope'.project_files()<CR>]], opts},
     {"n", "<leader>t", "<Cmd>NvimTreeToggle<CR>", opts},
     {"n", "c", [["_c]], opts},
     {"x", "c", [["_c]], opts},
@@ -150,7 +131,7 @@ local function set_theme()
   cmd([[colorscheme nord]])
 end
 
-set_globals()
 set_options()
+set_globals()
 set_mapping()
 set_theme()
