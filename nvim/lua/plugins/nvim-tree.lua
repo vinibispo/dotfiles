dcafa3ed782734b10e23aac59953fae9f4f6c892
vinibispo <vini.bispo015@gmail.com>
2021-09-29 06:11:52 -0300

    feat: add config to lsp on plugins.nvim-tree

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,52 @@
+require'nvim-tree'.setup {
+  -- disables netrw completely
+  disable_netrw = true,
+  -- hijack netrw window on startup
+  hijack_netrw = true,
+  -- open the tree when running this setup function
+  open_on_setup = true,
+  -- will not open on setup if the filetype is in this list
+  ignore_ft_on_setup = {},
+  -- closes neovim automatically when the tree is the last **WINDOW** in the view
+  auto_close = false,
+  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+  open_on_tab = false,
+  -- hijack the cursor in the tree to put it at the start of the filename
+  hijack_cursor = false,
+  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
+  update_cwd = false,
+  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+  update_focused_file = {
+    -- enables the feature
+    enable = false,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd = false,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {'.git', 'node_modules', '.cache'},
+  },
+  -- configuration options for the system open command (`s` in the tree by default)
+  system_open = {
+    -- the command to run this, leaving nil should work in most cases
+    cmd = nil,
+    -- the command arguments as a list
+    args = {},
+  },
+}
+vim.g.nvim_tree_gitignore = 1
+vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_hide_dotfiles = 1
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_root_folder_modifier = ':~'
+vim.g.nvim_tree_auto_resize = 0
+vim.g.nvim_tree_add_trailing = 1
+vim.g.nvim_tree_group_empty = 1
+vim.g.nvim_tree_lsp_diagnostics = 1
+vim.g.nvim_tree_disable_window_picker = 1
+vim.g.nvim_tree_icon_padding = ' '
+vim.g.nvim_tree_symlink_arrow = ' >> '
+vim.g.nvim_tree_respect_buf_cwd = 1
+vim.g.nvim_tree_refresh_wait = 500
