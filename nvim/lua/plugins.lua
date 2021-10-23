812f1476d0d132a83cb4607cedf22d9ca1beccfc
vinibispo <vini.bispo015@gmail.com>
2021-10-22 21:22:54 -0300

    refactor(nvim): replace shadmansaleh/lualine.nvim for nvim-lualine/lualine.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -111,7 +111,7 @@ return require("packer").startup(function(use)
     end,
   }
   use {
-    'shadmansaleh/lualine.nvim',
+    'nvim-lualine/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons', opt = true},
     config = function()
       require'lualine'.setup {theme = 'nord'}
