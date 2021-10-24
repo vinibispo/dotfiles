9a19daa8fc028ed033c9403cfc2c25d797b0964e
vinibispo <vini.bispo015@gmail.com>
2021-10-24 11:33:57 -0300

    chore(nvim): remove makeprg

--- nvim/lua/modules/languages.lua
+++ nvim/lua/modules/languages.lua
@@ -1,7 +1,5 @@
 local M = {}
 function M.js()
   vim.env.PATH = "node_modules/.bin:" .. vim.env.PATH
-  vim.bo.makeprg = "eslint -f compact %"
-  vim.opt.errorformat:append('%f:line%l,col%c,%m,%-G%.%#')
 end
 return M
