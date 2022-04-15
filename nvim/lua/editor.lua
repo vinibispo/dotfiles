52e46bc98f10c468bbde24ad1a7ff15deb790632
vinibispo <vini.bispo015@gmail.com>
2022-04-15 10:04:01 -0300

    feat(nvim): add so many configs in editor

--- nvim/lua/editor.lua
+++ nvim/lua/editor.lua
@@ -5,6 +5,21 @@ local cmd = vim.cmd
 
 local function set_globals()
   g.mapleader = ','
+  g.vimspector_enable_mappings = 'HUMAN'
+end
+local function tableMerge(t1, t2)
+  for k,v in pairs(t2) do
+    if type(v) == "table" then
+      if type(t1[k] or false) == "table" then
+        tableMerge(t1[k] or {}, t2[k] or {})
+      else
+        t1[k] = v
+      end
+    else
+      t1[k] = v
+    end
+  end
+  return t1
 end
 local function set_options()
   local undo_dir = os.getenv("HOME") .. '/.local/share/nvim/undo'
@@ -70,6 +85,10 @@ local function set_options()
     autocmd BufEnter *.json.jbuilder set filetype=ruby
   ]])
 
+  cmd([[
+    autocmd BufEnter .env.* set filetype=sh  ]])
+  cmd([[
+    autocmd BufEnter Dockerfile.* set filetype=dockerfile ]])
 end
 
 local function set_mapping()
@@ -79,7 +98,6 @@ local function set_mapping()
     {"s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts},
     {"i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts},
     {"s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts},
-    {"n", "<F5>", "<Cmd>e!<CR>", opts},
     {"n", "<leader>W", "<Cmd>w<CR>", opts},
     -- go window up
     {"n", "<leader>w", "<Cmd>wincmd k<CR>", opts},
@@ -128,8 +146,17 @@ local function set_mapping()
     {"x", "p", "pgvy", opts},
     {"n", "p", "p=`]", opts},
     {"n", "P", "P=`]", opts},
-
+    {"n", "<F5>", [[<Cmd>lua require 'dap'.continue()<CR>]], tableMerge(opts, {silent = true })},
+  {"n", "<F10>", [[<Cmd>lua require 'dap'.step_over()<CR>]], tableMerge(opts, {silent = true })},
+  {"n", "<F11>", [[<Cmd>lua require 'dap'.step_into()<CR>]], tableMerge(opts, {silent = true })},
+  {"n", "<F12>", [[<Cmd>lua require 'dap'.step_out()<CR>]], tableMerge(opts, {silent = true })},
+  {"n", "<F7>", [[<Cmd>lua require 'dap'.toggle_breakpoint()<CR>]], tableMerge(opts, {silent = true })},
+  {"n", "<leader>B", [[<Cmd>lua require 'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>]], tableMerge(opts, {silent = true })},
+  {"n", "<leader>lp", [[<Cmd>lua require 'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>]], tableMerge(opts, {silent = true })},
+  {"n", "<leader>dr", [[<Cmd>lua require 'dap'.repl.open()<CR>]], tableMerge(opts, {silent = true })},
+  {"n", "<leader>dl", [[<Cmd>lua require 'dap'.run_last()<CR>]], tableMerge(opts, {silent = true })}
   }
+
   for _, val in pairs(mappings) do
     api.nvim_set_keymap(unpack(val))
   end
