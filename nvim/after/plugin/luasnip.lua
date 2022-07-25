87dbced99d1e0ec86ca38bb3057ddc2abc211be4
vinibispo <vini.bispo015@gmail.com>
2022-07-24 21:35:27 -0300

    feat(nvim): add luasnip config

---
+++ nvim/after/plugin/luasnip.lua
@@ -0,0 +1,11 @@
+local luasnip = require('luasnip')
+luasnip.filetype_extend("ruby", { "rails" })
+luasnip.config.setup { history = true, store_selection_keys = "<Tab>", updateevents = "TextChanged, TextChangedI",
+delete_check_events = "TextChanged, TextChangedI" }
+
+local from_vscode = require('luasnip.loaders.from_vscode')
+from_vscode.lazy_load()
+
+for _, lang in pairs({ "ruby" }) do
+  luasnip.add_snippets(lang, require("snippets." .. lang), { key = lang })
+end
