c4f6bf520bf8dea3eb29d73c64ec7a1f84d96194
vinibispo <vini.bispo015@gmail.com>
2022-07-26 09:21:01 -0300

    feat(nvim): add mappings on editor

--- nvim/plugin/editor.lua
+++ nvim/plugin/editor.lua
@@ -1,27 +1,28 @@
 local function set_globals()
-	vim.g.mapleader = ","
+  vim.g.mapleader = ","
 end
+
 local function set_options()
-local undo_dir = os.getenv("HOME") .. '/.local/share/nvim/undo'
-local options = {
+  local undo_dir = os.getenv("HOME") .. "/.local/share/nvim/undo"
+  local options = {
     incsearch = true,
     ignorecase = true,
     encoding = "UTF-8",
-    ffs = 'unix,dos,mac',
+    ffs = "unix,dos,mac",
     modeline = true,
     autoindent = true,
     tabstop = 2,
     shiftwidth = 2,
     expandtab = true,
     smarttab = true,
-    backspace = 'indent,eol,start',
+    backspace = "indent,eol,start",
     laststatus = 2,
     relativenumber = true,
     vb = true,
     confirm = true,
     showmode = false,
     smartindent = true,
-    clipboard = 'unnamed,unnamedplus',
+    clipboard = "unnamed,unnamedplus",
     wrap = true,
     linebreak = true,
     foldmethod = "expr",
@@ -35,7 +36,7 @@ local options = {
     modifiable = true,
     foldlevelstart = 99,
     exrc = true,
-    guicursor = '',
+    guicursor = "",
     hidden = true,
     number = true,
     undodir = undo_dir,
@@ -45,21 +46,36 @@ local options = {
     cmdheight = 2,
     updatetime = 50,
     colorcolumn = "80",
-    background = 'dark',
-    termguicolors = true
+    background = "dark",
+    termguicolors = true,
   }
   for key, val in pairs(options) do
     vim.opt[key] = val
   end
 end
 
+local function set_mappings()
+  local opts = { noremap = true, silent = true }
+  local mappings = {
+    { "n", "F5", "<cmd>e!", opts },
+    { "n", "<leader>ev", "<cmd>e ~/dotfiles/nvim/init.lua <CR>", opts },
+    { "n", "<leader>sv", "<cmd>source ~/dotfiles/nvim/init.lua <CR>", opts },
+  }
+
+  for _, val in pairs(mappings) do
+    vim.keymap.set(unpack(val))
+  end
+end
+
 local function set_theme()
   vim.g.catppuccin_flavour = "latte"
-	vim.cmd([[ colorscheme catppuccin]])
+  vim.cmd([[ colorscheme catppuccin]])
 end
 
 set_globals()
 
 set_options()
+
+set_mappings()
 
 set_theme()
