57c9b1e85958f1f4d24673dd54c8a579bf2230af
vinibispo <vini.bispo015@gmail.com>
2022-07-27 08:51:48 -0300

    feat(nvim): split lsp.lua in mason.lua

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,7 +1,4 @@
-local mason_lspconfig = require("mason-lspconfig")
 local lsp = require("modules.lsp")
-
-local lspconfig = require("lspconfig")
 
 local saga = require("lspsaga")
 
@@ -22,21 +19,4 @@ null_ls.setup({
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.stylua,
   },
-})
-
-mason_lspconfig.setup_handlers({
-  function(name)
-    return lspconfig[name].setup(lsp.make_config())
-  end,
-  ["sumneko_lua"] = function()
-    local config = lsp.make_config()
-    config.settings = {
-      Lua = {
-        runtime = { version = "LuaJIT" },
-        diagnostics = { globals = { "vim", "use_rocks" } },
-      },
-    }
-
-    return lspconfig.sumneko_lua.setup(config)
-  end,
 })
--- nvim/plugin/mason.lua
+++ nvim/plugin/mason.lua
@@ -1,1 +1,22 @@
-require('mason').setup()
+require("mason").setup()
+
+local lspconfig = require("lspconfig")
+local mason_lspconfig = require("mason-lspconfig")
+local lsp = require("modules.lsp")
+
+mason_lspconfig.setup_handlers({
+  function(name)
+    return lspconfig[name].setup(lsp.make_config())
+  end,
+  ["sumneko_lua"] = function()
+    local config = lsp.make_config()
+    config.settings = {
+      Lua = {
+        runtime = { version = "LuaJIT" },
+        diagnostics = { globals = { "vim", "use_rocks" } },
+      },
+    }
+
+    return lspconfig.sumneko_lua.setup(config)
+  end,
+})
