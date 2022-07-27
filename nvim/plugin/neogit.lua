692006bd4a181359d8e096824de96f387b04e601
vinibispo <vini.bispo015@gmail.com>
2022-07-27 09:14:53 -0300

    feat(nvim): add mappings for neogit

---
+++ nvim/plugin/neogit.lua
@@ -0,0 +1,39 @@
+local neogit = require("neogit")
+
+local function setup()
+  neogit.setup({
+    kind = "split",
+    integrations = { diffview = true },
+  })
+end
+
+local function set_mappings()
+  local opts = { noremap = true, silent = true }
+  local mappings = {
+    {
+      "n",
+      "<leader>gc",
+      function()
+        neogit.open({ "commit" })
+      end,
+      opts,
+    },
+    {
+      "n",
+      "<leader>gp",
+      function()
+        neogit.open({ "push" })
+      end,
+      opts,
+    },
+    { "n", "<leader>gs", neogit.open, opts },
+  }
+
+  for _, mapping in pairs(mappings) do
+    vim.keymap.set(unpack(mapping))
+  end
+end
+
+setup()
+
+set_mappings()
