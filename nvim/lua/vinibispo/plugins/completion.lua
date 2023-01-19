1fbd631b3371f1fc08536c4b83554bdcf1d1dbd2
vinibispo <vini.bispo015@gmail.com>
2023-01-19 07:08:58 -0300

    fix(nvim): fix completion in neovim

--- nvim/lua/vinibispo/plugins/completion.lua
+++ nvim/lua/vinibispo/plugins/completion.lua
@@ -29,19 +29,17 @@ local function config()
       },
     },
     formatting = {
-      format = lspkind.cmp_format({
-        width_text = false,
-        max_width = 50,
-        menu = {
-          buffer = "[Buffer]",
-          nvim_lsp = "[LSP]",
-          luasnip = "[LuaSnip]",
-          nvim_lua = "[Lua]",
-          path = "[Path]",
-          treesitter = "[TS]",
-        },
-        documentation = { border = "single" },
-      }),
+      format = function(entry, vim_item)
+        if vim.tbl_contains({ "path" }, entry.source.name) then
+          local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+          if icon then
+            vim_item.kind = icon
+            vim_item.kind_hl_group = hl_group
+            return vim_item
+          end
+        end
+        return lspkind.cmp_format({ with_text = true })(entry, vim_item)
+      end,
     },
     mapping = {
       ["<C-n>"] = cmp.mapping(function(fallback)
