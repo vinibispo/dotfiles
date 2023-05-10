b9b21d82b9111b1afbd24013bd6647c2e2e39879
vinibispo <vini.bispo015@gmail.com>
2023-05-10 13:30:52 -0300

    feat(nvim): add nvim-treesitter-rescript

--- nvim/lua/vinibispo/plugins/nvim-treesitter.lua
+++ nvim/lua/vinibispo/plugins/nvim-treesitter.lua
@@ -3,17 +3,18 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "BufReadPost",
-    dependencies = { "nvim-treesitter/playground" },
+    dependencies = { "nvim-treesitter/playground", "nkrkv/nvim-treesitter-rescript" },
     init = function()
       vim.o.foldexpr = "nvim_treesitter#foldexpr()"
       vim.o.foldmethod = "expr"
+      vim.o.foldenable = false
     end,
     config = function()
       require("nvim-treesitter.configs").setup({
         highlight = { enable = true },
+        indent = { enable = true },
         incremental_selection = {
           enable = true,
-          indent = { enable = true },
         },
       })
     end,
